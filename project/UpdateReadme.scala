07c8293f6b25549eda4f3f8dfae5a87980611d3b
xuwei-k <6b656e6a69@gmail.com>
2026-06-17 08:14:48 +0900

    prepare sbt 2

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -6,12 +6,12 @@ object UpdateReadme {
 
   val updateReadmeTask = { (state: State) =>
     val extracted = Project.extract(state)
-    val v = extracted get version
-    val org =  extracted get organization
+    val v = extracted.get(version)
+    val org = extracted.get(organization)
     val modules = build.modules
     val readme = "README.md"
     val readmeFile = file(readme)
-    val newReadme = Predef.augmentString(IO.read(readmeFile)).lines.map{ line =>
+    val newReadme = IO.readLines(readmeFile).map{ line =>
       val matchReleaseOrSnapshot = line.contains("SNAPSHOT") == v.contains("SNAPSHOT")
       if(line.startsWith("libraryDependencies") && matchReleaseOrSnapshot){
         val i = modules.indexWhere(line.contains)
@@ -19,7 +19,7 @@ object UpdateReadme {
       }else line
     }.mkString("", "\n", "\n")
     IO.write(readmeFile, newReadme)
-    val git = new Git(extracted get baseDirectory)
+    val git = new Git(extracted.get(baseDirectory))
     git.add(readme) ! state.log
     git.commit(message = "update " + readme, sign = false, signOff = false) ! state.log
     sys.process.Process("git diff HEAD^") ! state.log
